18ff06e9c9a5f7f77e0002efe092996e4c4ba09c
pinoelefante <pinoelefante@hotmail.it>
2014-10-06 22:04:25 +0200

    layouts

---
+++ src/it/pinoelefante/mycustomviews/PLinearLayout.java
@@ -0,0 +1,29 @@
+package it.pinoelefante.mycustomviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class PLinearLayout extends LinearLayout {
+
+	public PLinearLayout(Context context) {
+		super(context);
+	}
+
+	public PLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public void setSizePerc(int w, int h){
+		int h_screen = getResources().getDisplayMetrics().heightPixels;
+		int w_screen = getResources().getDisplayMetrics().widthPixels;
+		
+		int w_p=(w_screen/100)*w;
+		int h_p=(h_screen/100)*h;
+		if(getLayoutParams()==null){
+			setLayoutParams(new LayoutParams(w_p, h_p));
+		}
+		getLayoutParams().height=h_p;;
+		getLayoutParams().width=w_p;;
+	}
+}
---
+++ src/it/pinoelefante/mycustomviews/PRelativeLayout.java
@@ -0,0 +1,32 @@
+package it.pinoelefante.mycustomviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class PRelativeLayout extends RelativeLayout {
+
+	public PRelativeLayout(Context context) {
+		super(context);
+	}
+
+	public PRelativeLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public PRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	public void setSizePerc(int w, int h){
+		int h_screen = getResources().getDisplayMetrics().heightPixels;
+		int w_screen = getResources().getDisplayMetrics().widthPixels;
+		
+		int w_p=(w_screen/100)*w;
+		int h_p=(h_screen/100)*h;
+		if(getLayoutParams()==null){
+			setLayoutParams(new LayoutParams(w_p, h_p));
+		}
+		getLayoutParams().height=h_p;;
+		getLayoutParams().width=w_p;;
+	}
+}
